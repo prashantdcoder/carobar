90bffe34dd35bf411941e49ceb433573f4fbf8e5
Prashant Kumar <prashantdcoder@gmail.com>
2019-12-25 22:56:56 +0530

    # refactore seller UI

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -83,8 +83,8 @@ grails {
     mail {
         host = "smtp.gmail.com"
         port = 465
-        username = "rhlsngh994@gmail.com"
-        password = "elzardogmail"
+        username = ""
+        password = ""
         props = ["mail.smtp.auth":"true",
                  "mail.smtp.socketFactory.port":"465",
                  "mail.smtp.socketFactory.class":"javax.net.ssl.SSLSocketFactory",
